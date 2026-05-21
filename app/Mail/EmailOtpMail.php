89ae42ad3c01de5f161a145e5bad2813e877befc
Naderadelp <naderadelpp@gmail.com>
2026-05-21 17:23:21 +0300

    feat: implement ShouldQueue interface in EmailOtpMail to enable asynchronous email delivery

--- app/Mail/EmailOtpMail.php
+++ app/Mail/EmailOtpMail.php
@@ -4,12 +4,13 @@ namespace App\Mail;
 
 use App\Models\EmailOtp;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class EmailOtpMail extends Mailable
+class EmailOtpMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
